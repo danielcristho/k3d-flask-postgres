1e924e76c8a4072394a55ebe1cbf1cfa9c1163e5
danielcristho <pepuhodaniel93.learn@gmail.com>
2024-12-17 22:26:29 +0700

    feat: update cluster setup

--- vagrant-infra/k3s-worker.sh
+++ vagrant-infra/k3s-worker.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
+# Get the master node's IP from the arguments
+MASTER_IP=$1
+
 # Get the token from the shared folder
-TOKEN=$(cat /vagrant/token)
+TOKEN=$(sudo cat /vagrant/token)
 
 # Install K3s agent (worker) and join the master node
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.20:6443 K3S_TOKEN=$TOKEN sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://$MASTER_IP:6443 K3S_TOKEN=$TOKEN sh -
 
-# install docker
-curl -fsSL https://get.docker.com -o install-docker.sh
-sudo sh install-docker.sh
+# Get head config and replace with head IP
+mkdir -p $HOME/.kube
+cp /vagrant/config $HOME/.kube/config
+sed -i "s|server: https://127.0.0.1:6443|server: https://$MASTER_IP:6443|g" $HOME/.kube/config
 
-# set rootless
-sudo sh -eux <<EOF
-# Install newuidmap & newgidmap binaries
-apt-get install -y uidmap
-# Add subuid entry for vagrant
-echo "vagrant:100000:65536" >> /etc/subuid
-# Add subgid entry for vagrant
-echo "vagrant:100000:65536" >> /etc/subgid
-EOF
-dockerd-rootless-setuptool.sh install
+# Install Docker
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker vagrant
